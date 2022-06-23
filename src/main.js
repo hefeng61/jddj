e4499f184df551dc4c5fcc447a9804a8c6bcacec
hefeng <aaron@hbtgft.com>
2022-06-23 17:28:54 +0800

    home

--- src/main.js
+++ src/main.js
@@ -2,5 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import 'normalize.css'
+import '@icon-park/vue-next/styles/index.css'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+app.use(store).use(router).mount('#app')
